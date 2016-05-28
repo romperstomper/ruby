0228e08d9b688d0a6551a3a6c62c5de2a98b784e
gary boland <garyboland@gmail.com>
2016-05-29 00:24:53 +0100

    app.rb

---
+++ app.rb
@@ -0,0 +1,50 @@
+require 'json'
+path = File.join(File.dirname(__FILE__), '../data/products.json')
+file = File.read(path)
+products_hash = JSON.parse(file)
+
+# Print today's date
+puts Time.now
+puts "                     _            _       "
+puts "                    | |          | |      "
+puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
+puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
+puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
+puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
+puts "| |                                       "
+puts "|_|                                       "
+
+
+def calculate_purchases(toy)
+  count = 0
+  toy["purchases"].each do |amount|
+    puts amount['price']
+  end
+  return count
+end
+# For each product in the data set:
+  # Print the name of the toy
+products_hash["items"].each do |toy|
+  puts toy["title"] + " " + toy["full-price"] 
+end
+  puts products_hash["items"][0]
+  # Print the retail price of the toy
+  # Calculate and print the total number of purchases
+  # Calculate and print the total amount of sales
+  # Calculate and print the average price the toy sold for
+  # Calculate and print the average discount (% or $) based off the average sales price
+
+
+	puts " _                         _     "
+	puts "| |                       | |    "
+	puts "| |__  _ __ __ _ _ __   __| |___ "
+	puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
+	puts "| |_) | | | (_| | | | | (_| \\__ \\"
+	puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
+	puts
+
+# For each brand in the data set:
+  # Print the name of the brand
+  # Count and print the number of the brand's toys we stock
+  # Calculate and print the average price of the brand's toys
+  # Calculate and print the total revenue of all the brand's toy sales combined
