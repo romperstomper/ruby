d8a767b3c16cb511afe7894ab9faf7424d003ee5
gary boland <garyboland@gmail.com>
2016-08-13 17:57:10 +0100

    simonpig

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
 #write your code here
+def echo(args)
+  return args
+end
+
+def shout(args)
+  return args.upcase
+end
+
+def repeat(args, num=0)
+  if num == 0
+    return args + ' ' + args
+  end
+  r = ' ' + args
+  args + (r*(num-1))
+end
+
+def start_of_word(args, num)
+  return args[0..num-1]
+end
+
+def first_word(args)
+  return args.split[0]
+end
+
+def titleize(args)
+  res = [args.split[0].capitalize]
+  args.split[1..-1].each do |x|
+    puts "value? #{x}"
+    x = x.capitalize unless x == 'and'|| x == 'the'||x=='over'
+    res.push(x) 
+  end
+  res.join(' ')
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,32 @@
-#write your code here
+#wjite your code here
+def translate(args)
+  v = %w[a e i o u]
+  s = 0
+  w = 0
+  chars = args
+  suf = 'ay'
+  res = ''
+  result = []
+  sargs = args.split
+  sargs.each do |arr|
+    k = 0
+    c = 0
+    arr.each_char do |x|
+      if arr.include?('qu') && k == arr.index('qu')
+          res += 'qu'
+          k += 1
+          c += 2
+      elsif x.start_with?(*v)
+        result[w] = arr[c..-1] + res + suf 
+        w += 1
+        res = ''
+        break
+      else
+        res += x
+        c +=1
+        k +=1
+      end
+    end
+  end
+  return result.join(" ")
+end
