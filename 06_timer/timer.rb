a50ca094b7e194af8ae4c1d3f175623176829f97
gary boland <garyboland@gmail.com>
2016-08-14 13:22:42 +0100

    timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,16 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  attr_accessor :minutes
+  def initialize(secs=0)
+    @seconds = secs
+  end
+
+  def time_string
+    seconds = @seconds % 60
+    h=(@seconds/3600).to_s.rjust(2, '0')
+    m = ((@seconds/60)%60).to_s.rjust(2, '0')
+    s = seconds.to_s.rjust(2, '0')
+    "#{h}:#{m}:#{s}"
+  end
+
 end
